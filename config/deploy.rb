2c450425efbf077ceb706904bc471394606a3897
jaischeema <jaischeema@gmail.com>
2011-11-05 10:50:12 +1100

    Adding the Capistrano stuff

---
+++ config/deploy.rb
@@ -0,0 +1,18 @@
+set :application, "www.apnavirsa.net"
+set :repository,  "git@github.com:jaischeema/apnavirsa.git"
+
+set :scm, :git
+
+set :deploy_to, "/home/jais/production/www.apnavirsa.net"
+
+server "10.1.1.222", :app, :web, :db, :primary => true
+
+default_run_options[:pty] = true
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
